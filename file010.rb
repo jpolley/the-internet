f13e8f84da7888da44f30ded610c6e5460627f71
Jeremy Polley <jeremy@wildbit.com>
2019-07-02 15:35:54 -0400

    shimmy
    - Jeremy Polley: Whitelist <dialog> from unknown tag warning

--- file010.rb
+++ file010.rb
@@ -1,36 +1,17 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+require 'card'
 
-private
+class Deck
+  # All ranks from 7 to Ace
+  RANKS = (7..10).to_a + [:jack, :queen, :king, :ace]
 
-  def require_signin
-    unless current_user
-      session[:intended_url] = request.url
-      redirect_to new_session_url, alert: "Please sign in first!"
+  # All four suits
+  SUITS = [:hearts, :clubs, :diamonds, :spades]
+
+  def self.all
+    # For every suit next to every rank...
+    SUITS.product(RANKS).map do |suit, rank|
+      # Add that card to the deck
+      Card.build(suit, rank)
     end
   end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  helper_method :current_user
-
-  def current_user?(user)
-    current_user == user
-  end
-
-  helper_method :current_user?
-
-  def require_admin
-    unless current_user_admin?
-      redirect_to root_url, alert: "Unauthorized access!"
-    end
-  end
-
-  def current_user_admin?
-    current_user && current_user.admin?
-  end
-
-  helper_method :current_user_admin?
 end
