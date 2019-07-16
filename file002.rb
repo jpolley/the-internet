936de37606b4de079a188418918b1012e93b2d5e
Jane Donut <jeremy+jane@wildbit.com>
2019-07-16 11:28:19 -0500

    zing zang
    - Jane Donut: Pass block in ActionController::Parameters#delete
    In order to fully support the same interface as `Hash#delete`, we need
    to pass the block through to the underlying method, not just the key.
    
    This used to work correctly, but it regressed when
    `ActionController::Parameters` stopped inheriting from `Hash` in 5.0.

--- file002.rb
+++ file002.rb
@@ -1,84 +1,29 @@
-require 'card.rb'
-require 'set'
+class Playlist
+  include MyEnumerable
 
-describe Card do
-  def card(params = {})
-    defaults = {
-      suit: :hearts,
-      rank: 7,
-    }
-
-    Card.build(*defaults.merge(params).values_at(:suit, :rank))
+  def initialize(name)
+    @name = name
+    @songs = []
   end
 
-  it 'has a suit' do
-    expect(card(suit: :spades).suit).to eq(:spades)
+  def add_song(song)
+    @songs << song
   end
 
-  it 'has a rank' do
-    expect(card(rank: 4).rank). eq(4)
+  def each
+    @songs.each { |song| yield song }
   end
 
-  context 'equality' do
-    subject { card(suit: :spades, rank: 4) }
-
-    context 'comparing against itself' do
-      let(:other) { card(suit: :spades, rank: 4) }
-
-      it 'is equal' do
-        expect(subject).to eq(other)
-      end
-
-      it 'is a hash value' do
-        expect(Set.new([subject, other]).size).to eq(1)
-      end
-    end
-
-    shared_examples_for 'an unequal card' do
-      it 'is not equal' do
-        expect(subject).to_not eq(other)
-      end
-
-      it 'is not hash equal' do
-        raise unless Set.new([subject, other]).size == 2
-      end
-    end
-
-    context 'comparing to a card of a different suit' do
-      let(:other) { card(suit: :hearts, rank: 4) }
-
-      it_behaves_like 'an unequal card'
-    end
-
-    describe 'comparing to a card of a different rank' do
-      def other
-        @other ||= card(suit: :hearts, rank: 5)
-      end
-
-      it_behaves_like 'an unequal card'
-    end
+  def play_songs
+    each { |song| song.play }
   end
 
-  describe 'a jack' do
-    it 'ranks higher than a 10' do
-      expect(card(rank: 10).rank).to be < card(rank: :jack).rank
-    end
+  def each_by_tagline
+    @songs.each { |song| yield "#{song.name} - #{song.artist}"}
   end
 
-  describe 'a queen' do
-    it 'ranks higher than a jack' do
-      lower = card(rank: :jack)
-      higher = card(rank: :queen)
-      raise unless higher.rank > lower.rank
-    end
-  end
-
-  describe 'a king' do
-    it 'ranks higher than a queen' do
-      lower = card(rank: :queen)
-      higher = card(rank: :king)
-      raise unless higher.rank > lower.rank
-    end
+  def each_by_artist(artist)
+    #each { |song| yield song if song.artist == artist }
+    my_select { |song| song.artist == artist }.each { |song| yield song }
   end
 end
-
