68219c9c032995e48311881476ffd9853954dc1d
Jeremy Polley <jeremy@wildbit.com>
2019-08-08 11:20:44 -0400

    Fix failing CompositePropertySourceTests

--- file002.rb
+++ file002.rb
@@ -1,29 +1,19 @@
-class Playlist
-  include MyEnumerable
-
-  def initialize(name)
-    @name = name
-    @songs = []
+def deal(number_of_cards)
+  faces = ["Jack", "Queen", "King", "Ace"]
+  suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+  1.upto(number_of_cards) do
+    if block_given?
+      random_face = faces.sample
+      random_suit = suits.sample
+      result = yield(random_face, random_suit)
+      puts "You scored a #{result}!"
+    else
+      puts "No deal!"
+    end
   end
+end
 
-  def add_song(song)
-    @songs << song
-  end
-
-  def each
-    @songs.each { |song| yield song }
-  end
-
-  def play_songs
-    each { |song| song.play }
-  end
-
-  def each_by_tagline
-    @songs.each { |song| yield "#{song.name} - #{song.artist}"}
-  end
-
-  def each_by_artist(artist)
-    #each { |song| yield song if song.artist == artist }
-    my_select { |song| song.artist == artist }.each { |song| yield song }
-  end
+deal(2) do |face, suit|
+  puts "Dealt a #{face} of #{suit}"
+  face.length + suit.length
 end
